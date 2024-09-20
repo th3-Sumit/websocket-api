45239da698d88c37c124835df061e14f4edc5a71
th3_Spoil3r <sumit@weboappdiscovery.com>
2024-09-20 16:19:27 +0530

    Added connected  user count for group ...!!

--- index.js
+++ index.js
@@ -25,23 +25,49 @@ app.get('/', (req, res) => {
 io.on('connection', onConnected)
 
 function onConnected(socket){
-    console.log(`New Connection: ${socket.id}`)
+    // console.log(`New Connection: ${socket.id}`)
 
-    socket.on('join_room', (room_info) => {
-        socket.join(room_info.room)
-        console.log(`User Id :- ${socket.id} joined room : ${room_info.room}`)
-    })
+    // socket.on('join_room', (room_info) => {
+    //     socket.join(room_info.room)
+    //     console.log(`User Id :- ${socket.id} joined room : ${room_info.room}`)
+    // })
 
-    socket.on('send_message', (data) => {
-        console.log(`data send`, data)
-        socket.to(data?.room).emit("receive_message", data)
-    })
+    // socket.on('send_message', (data) => {
+    //     console.log(`data send`, data)
+    //     socket.to(data?.room).emit("receive_message", data)
+    // })
 
-    // socket.to()
+    // io.on('disconnect', () => {
+    //     console.log(`User Disconnected: ${socket.id}`)
+    // })
 
-    io.on('disconnect', () => {
-        console.log(`User Disconnected: ${socket.id}`)
-    })
+      socket.on('join_room', (room_info) => {
+          socket.join(room_info.room);
+          console.log(`User Id: ${socket.id} joined room: ${room_info.room}`);
+  
+          // Get the number of clients in the room
+          const roomUsers = io.sockets.adapter.rooms.get(room_info.room)?.size || 0;
+  
+          // Emit the updated user count to all users in the room
+          io.to(room_info.room).emit('room_user_count', roomUsers);
+      });
+  
+      socket.on('send_message', (data) => {
+        
+          console.log('data send', data);
+          socket.to(data.room).emit('receive_message', data);
+      });
+  
+      socket.on('disconnect', () => {
+          console.log(`User Disconnected: ${socket.id}`);
+          // delete socket.rooms(socket.id)
+          // Broadcast updated user count to all rooms (since the user left)
+          socket.rooms.forEach((room) => {
+              const roomUsers = io.sockets.adapter.rooms.get(room)?.size || 0;
+              io.to(room).emit('room_user_count', roomUsers);
+          });
+      });
+  
 }
 
 server.listen(port, () => {
